27c583b166d36a361b11c7d0a8cae1628e3d1445
bruno <bruno.santos@ifpi.edu.br>
2020-03-03 23:37:37 -0300

    modificações

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/pages/login.dart';
 
+import 'pages/login.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Instagram-Clone',
-      home: Login()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Instagram-Clone',
+        home: Login());
   }
 }
-
-
-
